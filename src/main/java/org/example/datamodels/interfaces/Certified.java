c9bee2b29093824992fb7edaf6655fd37680efe9
Joso1207 <joso1207@student.miun.se>
2025-10-31 18:45:23 +0100

    making sure to follow ISP,  The Certified interface does not need to extend person.
    Example,  What if something other than a  person has certificates? (like a vehicle).  objects implementing person rather should just implement both interfaces instead

--- src/main/java/org/example/datamodels/interfaces/Certified.java
+++ src/main/java/org/example/datamodels/interfaces/Certified.java
@@ -6,6 +6,6 @@ import java.util.Set;
 //Another case of Interface segregation, we only add this to classes with the certifications set,
 //This is going to be candidates but might be expanded to include classes who use the person interface but not the ICandidate
 
-public interface Certified extends Person {
+public interface Certified {
     Set<String> certificates();
 }
